7f1c113b9229d37d96458800eed6a55516157680
Roman Razilov <roman.razilov@philips.com>
2014-12-10 15:59:04 +0100

    RR: Some fixes

--- FPIntroduction/Program.fs
+++ FPIntroduction/Program.fs
@@ -21,11 +21,13 @@ let add1mul2 x = mul 2 (add 1 x)
 // data transformation
 let add2mul3 x = x |> add 2 |> mul 3
 
+
+let s x = mul 3  (add 2 x) 
 // let (|>) x f = f x
 
 
 // pipeline building
-let add4mul5 x =  add 2 >> mul 3
+let add4mul5 =  add 2 >> mul 3
 
 // let (>>) x f v = f (x v)
 
@@ -68,10 +70,6 @@ let formatIHave singular plural = format singular plural >> sprintf "I have %s"
 
 let formatIHaveApples = formatIHave "apple" "apples"
 
-// closures 
-let accumulator op n = 
-    let acc = ref (n)
-    fun i -> acc := op !acc i; !acc   
 
 // guards
 let formatPositive singular plural b = 
@@ -79,7 +77,7 @@ let formatPositive singular plural b =
     | 0 -> sprintf "no %s" plural
     | 1 -> sprintf "one %s" singular
     | 2 -> sprintf "two %s" plural
-    | _ as x when x < 0 -> failwith "negative is not allowed!" 
+    | x when x < 0 -> failwith "negative is not allowed!" 
     | x -> sprintf "%d %s" x plural
 
 let formatIHavePositive singular plural = formatPositive singular plural >> sprintf "I have %s" 
@@ -87,8 +85,45 @@ let formatIHavePositive singular plural = formatPositive singular plural >> spri
 let formatIHavePositiveApples = formatIHavePositive "apple" "apples"
 
 
+type Counts = Zero | One | Two | Many
+
+
+type Result = 
+    | Value of int
+    | Error of string
+
+let div x y = 
+    if y <> 0 then
+        Value (x / y)
+    else
+        Error "division by 0!"
+
+type Shape =
+    | Rectangle of float * float
+    | Circle of float
+
+let surfaceOfShape s = 
+    match s with
+    | Circle r -> System.Math.PI * r * r
+    | Rectangle (w,h) -> w * h
+
+type Shape with
+    member s.Surface = surfaceOfShape s
+
+type AssetId = AssetId of int
+
+type AssetId with
+    member x.toInt = let (AssetId i) = x in i
+
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
-    0 // return an integer exit code
+    0 
+    
+    
+// return an integer exit code
+// closures 
+//let accumulator op n = 
+//    let acc = ref (n)
+//    fun i -> acc := op !acc i; !acc   
